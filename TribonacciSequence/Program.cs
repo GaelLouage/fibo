c9c0716861866f8200e18063c67ccb4f9dda7cd8
Gaël Louage <gaellouage@hotmail.com>
2025-06-10 12:34:37 +0200

    Fibonacci Sequence Generation Using Iteration

---
+++ TribonacciSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fibo = Fibonnacci(10);
+        foreach(var n in fibo)
+        {
+            Console.WriteLine(n);
+        }
+    }
+
+    public static int[] Fibonnacci(int n)
+    {
+        var result = new List<int>();
+        var one = 0; 
+        var two = 1;
+        var temp = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            result.Add(one);
+            temp = one;
+            one = two; 
+            two = one + temp; 
+        }
+        return result.ToArray();
+    }
+}
